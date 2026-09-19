a5eabf3806cd6fe46dfc1d5b66b59d52c601057d
Robert Pliszka <robplisz@gmail.com>
2026-09-19 18:03:25 -0400

    Created loops to have each turtle move a different distance. Then the turtle that got first to the x coordinates wins the race

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 from turtle import Turtle, Screen
+import random
 
+is_race_on = False
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 y_position = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
 
 for turtle in range(0 , 6):
-    tim = Turtle(shape="turtle")
-    tim.color(colors[turtle])
-    tim.penup()
-    tim.goto(x=-230,y=y_position[turtle])
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_position[turtle])
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You win! The {winning_color} turtle wins!")
+            else:
+                print(f"You lose! The {winning_color} turtle wins!")
+
+        random_distance = random.randint(0,10)
+        turtle.forward(random_distance)
 
 
 screen.exitonclick()
